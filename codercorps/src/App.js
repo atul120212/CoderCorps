4ed19d6bccc6ac28f778214b5dadfe02a9674eac
AtulSharma <atulsharma28092004@gmail.com>
2022-12-23 06:51:39 +0530

    hanime

--- codercorps/src/App.js
+++ codercorps/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Zoom from 'react-reveal/Zoom';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>
+        <div className='headname'>
+        <Zoom left cascade>
+          Coder
+        </Zoom>
+        <Zoom right cascade> Corps </Zoom>
+        </div>
+      </h1>
         <h1>Welcome to Coder Corps Community</h1>
       </header>
     </div>
