9c69291972b412e56e5ad6ae8f9bb07a1d67e4bb
AtulSharma <atulsharma28092004@gmail.com>
2023-01-30 23:49:44 +0530

    codercorps

---
+++ lib/widgets/bottom_bar_column.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class BottomBarColumn extends StatelessWidget {
+  final String heading;
+  final String s1;
+  final String s2;
+  final String s3;
+
+  BottomBarColumn({
+    required this.heading,
+    required this.s1,
+    required this.s2,
+    required this.s3,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            heading,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 15,
+              //fontWeight: FontWeight.w500,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+
+          Text(
+              s1,
+              style: TextStyle(
+                color: Colors.white60,
+                fontSize: 13,
+                
+              ),
+            ),
+
+            Text(
+              s2,
+              style: TextStyle(
+                color: Colors.white60,
+                fontSize: 13,
+                
+              ),
+            ),
+
+            Text(
+              s3,
+              style: TextStyle(
+                color: Colors.white60,
+                fontSize: 13,
+                
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
